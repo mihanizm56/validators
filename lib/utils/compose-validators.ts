f774cbb6493eecb11e00c44140b99ccddba5dee2
mihanizm56 <m727507.56@mail.ru>
2020-04-01 03:03:16 +0300

    feat(update-package): upd types

--- lib/utils/compose-validators.ts
+++ lib/utils/compose-validators.ts
@@ -1,11 +1,11 @@
-import { Validator } from '@/types';
+import { Validator, ValidatorReturnsType } from '@/types';
 
 export const composeValidators = (validators: Array<Validator>) => (
   value?: string | number | Record<string, any> | Array<any>,
-): string =>
+) =>
   validators.reduce((prevError, validator) => {
     if (!Boolean(prevError)) {
-      const { error, errorTextValue } = validator(value);
+      const { error, errorTextValue }: ValidatorReturnsType = validator(value);
 
       if (error) {
         return errorTextValue;
