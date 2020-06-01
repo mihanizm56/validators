70966c1e9ba9ef55d66a11e6e64ebf94a7eda3f4
mihanizm56 <m727507.56@mail.ru>
2020-06-01 14:27:49 +0300

    build(configure-package): added ability to set custom error text

--- lib/types/pattern-validators.ts
+++ lib/types/pattern-validators.ts
@@ -1,13 +1,21 @@
 import { CustomPatternValidateParams, ValidatorReturnsType } from './index';
 
 export abstract class IPatternValidator {
-  abstract numbersOnly: (value?: string) => ValidatorReturnsType;
+  abstract numbersOnly: (
+    errorTextValue?: string,
+  ) => (value?: string) => ValidatorReturnsType;
 
-  abstract cyrillicsOnly: (value?: string) => ValidatorReturnsType;
+  abstract cyrillicsOnly: (
+    errorTextValue?: string,
+  ) => (value?: string) => ValidatorReturnsType;
 
-  abstract decimalNumbers: (value?: string) => ValidatorReturnsType;
+  abstract decimalNumbers: (
+    errorTextValue?: string,
+  ) => (value?: string) => ValidatorReturnsType;
 
-  abstract email: (value?: string) => ValidatorReturnsType;
+  abstract email: (
+    errorTextValue?: string,
+  ) => (value?: string) => ValidatorReturnsType;
 
   abstract customPatternValidate: (
     params: CustomPatternValidateParams,
--- lib/validators/pattern-validator/index.ts
+++ lib/validators/pattern-validator/index.ts
@@ -9,57 +9,74 @@ import {
   phoneNumber,
   decimalNumbers,
   datePattern,
-  isEmptyPattern,
 } from '@/patterns';
 
 export class PatternValidator extends IPatternValidator {
-  numbersOnly = (value?: string) =>
+  numbersOnly = (errorTextValue?: string) => (value?: string) =>
     getIsValueMatchPattern({
       value,
       pattern: simpleNumbersOnly,
     })
       ? { error: false, errorTextValue: '' }
-      : { error: true, errorTextValue: validationErrors.incorrect };
+      : {
+          error: true,
+          errorTextValue: errorTextValue || validationErrors.incorrect,
+        };
 
-  cyrillicsOnly = (value?: string) =>
+  cyrillicsOnly = (errorTextValue?: string) => (value?: string) =>
     getIsValueMatchPattern({
       value,
       pattern: cyrrillicsOnly,
     })
       ? { error: false, errorTextValue: '' }
-      : { error: true, errorTextValue: validationErrors.incorrect };
+      : {
+          error: true,
+          errorTextValue: errorTextValue || validationErrors.incorrect,
+        };
 
-  decimalNumbers = (value?: string) =>
+  decimalNumbers = (errorTextValue?: string) => (value?: string) =>
     getIsValueMatchPattern({
       value,
       pattern: decimalNumbers,
     })
       ? { error: false, errorTextValue: '' }
-      : { error: true, errorTextValue: validationErrors.incorrect };
+      : {
+          error: true,
+          errorTextValue: errorTextValue || validationErrors.incorrect,
+        };
 
-  email = (value?: string) =>
+  email = (errorTextValue?: string) => (value?: string) =>
     getIsValueMatchPattern({
       value,
       pattern: emailPattern,
     })
       ? { error: false, errorTextValue: '' }
-      : { error: true, errorTextValue: validationErrors.incorrect };
+      : {
+          error: true,
+          errorTextValue: errorTextValue || validationErrors.incorrect,
+        };
 
-  phoneNumber = (value?: string) =>
+  phoneNumber = (errorTextValue?: string) => (value?: string) =>
     getIsValueMatchPattern({
       value,
       pattern: phoneNumber,
     })
       ? { error: false, errorTextValue: '' }
-      : { error: true, errorTextValue: validationErrors.incorrect };
+      : {
+          error: true,
+          errorTextValue: errorTextValue || validationErrors.incorrect,
+        };
 
-  dateOnly = (value?: string) =>
+  dateOnly = (errorTextValue?: string) => (value?: string) =>
     getIsValueMatchPattern({
       value,
       pattern: datePattern,
     })
       ? { error: false, errorTextValue: '' }
-      : { error: true, errorTextValue: validationErrors.incorrect };
+      : {
+          error: true,
+          errorTextValue: errorTextValue || validationErrors.incorrect,
+        };
 
   customPatternValidate = ({
     pattern,
