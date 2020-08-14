e8c0e9a544b1a734b178c0b5a254aa91e240a853
mihanizm56 <m727507.56@mail.ru>
2020-08-14 03:26:05 +0300

    build(configure-package): v0.1.3

--- lib/patterns/index.ts
+++ lib/patterns/index.ts
@@ -1,7 +1,7 @@
 export const simpleNumbersOnly = /^\d+$/;
 export const decimalNumbers = /^\d*(\.)?\d{1,20}$/;
 export const cyrrillicsOnly = /^[ а-яА-ЯёЁ]+$/;
-export const emailPattern = /^[a-zA-Z0-9_.+-]{1,20}@[a-zA-Z0-9-]{1,10}\.[a-zA-Z0-9-.]{1,10}$/;
+export const emailPattern = /^[a-zA-Z0-9_.+-]{1,20}@[a-zA-Z0-9-]{1,20}\.[a-zA-Z0-9-.]{1,10}$/;
 export const phoneNumber = /^\+?\d(\(\d{0,3}\)|\d{0,3})(\d{3})(-(\d{2})-(\d{2})|(\d{4}))$/;
 export const datePattern = /^\d{2}\.\d{2}\.\d{4}$/;
 export const isEmptyPattern = /\w+/g;
