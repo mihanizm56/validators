34215ff9969c43ac11968bf962d3499156dda95f
mihanizm56 <m727507.56@mail.ru>
2020-10-08 16:40:23 +0300

    build(configure-package): fix empty numbers and cyrrilics

--- lib/patterns/index.ts
+++ lib/patterns/index.ts
@@ -1,6 +1,6 @@
-export const simpleNumbersOnly = /^\d+$/;
+export const simpleNumbersOnly = /^\d*$/;
 export const decimalNumbers = /^\d*(\.)?\d{1,20}$/;
-export const cyrrillicsOnly = /^[ а-яА-ЯёЁ]+$/;
+export const cyrrillicsOnly = /^[ а-яА-ЯёЁ]*$/;
 export const emailPattern = /^[a-zA-Z0-9_.+-]{1,20}@[a-zA-Z0-9-]{1,20}\.[a-zA-Z0-9-.]{1,10}$/;
 export const phoneNumber = /^\+?\d(\(\d{0,3}\)|\d{0,3})(\d{3})(-(\d{2})-(\d{2})|(\d{4}))$/;
 export const datePattern = /^\d{2}\.\d{2}\.\d{4}$/;
