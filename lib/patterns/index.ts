f9052944462636e57ee111bc96b4ab2efa41efb5
mihanizm56 <m727507.56@mail.ru>
2021-07-12 03:10:39 +0300

    feat(scripts): update email pattern

--- lib/patterns/index.ts
+++ lib/patterns/index.ts
@@ -1,7 +1,9 @@
 export const simpleNumbersOnly = /^\d*$/;
 export const decimalNumbers = /^\d*(\.)?\d{1,20}$/;
 export const cyrrillicsOnly = /^[ а-яА-ЯёЁ]*$/;
-export const emailPattern = /^[a-zA-Z0-9_.+-]{1,20}@[a-zA-Z0-9-]{1,20}\.[a-zA-Z0-9-.]{1,10}$/;
+// Standart email validation that matches almost all email formats
+// eslint-disable-next-line
+export const emailPattern = /^[a-zA-Zа-яА-Я0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Zа-яА-Я0-9](?:[a-zA-Zа-яА-Я0-9-]{0,61}[a-zA-Zа-яА-Я0-9])?(?:\.[a-zA-Zа-яА-Я0-9](?:[a-zA-Zа-яА-Я0-9-]{0,61}[a-zA-Zа-яА-Я0-9])?)*$/;
 export const phoneNumber = /^\+?\d(\(\d{0,3}\)|\d{0,3})(\d{3})(-(\d{2})-(\d{2})|(\d{4}))$/;
 export const datePattern = /^\d{2}\.\d{2}\.\d{4}$/;
 export const isEmptyPattern = /\w+/g;
