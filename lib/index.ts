853d1ba1a7620de5d6dabe7a528c05d185e65e67
mihanizm56 <m727507.56@mail.ru>
2022-03-21 02:46:25 +0500

    fix(update-package): fix types

--- lib/index.ts
+++ lib/index.ts
@@ -1,20 +1,13 @@
-
-export {PatternValidator} from '@/validators/pattern-validator'
-export {SimpleValidator} from '@/validators/simple-validator'
-
+export { PatternValidator } from '@/validators/pattern-validator';
+export { SimpleValidator } from '@/validators/simple-validator';
+export { ValidatorReturnsType, Validator } from '@/types';
+export { composeValidators } from './utils/compose-validators';
+export { getIsValueMatchPattern } from './utils/get-value-is-match-pattern';
 export {
-    ValidatorReturnsType,
-} from '@/types'
-
-export {composeValidators} from './utils/compose-validators'
-
-export {getIsValueMatchPattern} from './utils/get-value-is-match-pattern'
-
-export {
-    simpleNumbersOnly,
-    decimalNumbers,
-    cyrrillicsOnly,
-    emailPattern,
-    phoneNumber,
-    datePattern
-} from './patterns'
+  simpleNumbersOnly,
+  decimalNumbers,
+  cyrrillicsOnly,
+  emailPattern,
+  phoneNumber,
+  datePattern,
+} from './patterns';
