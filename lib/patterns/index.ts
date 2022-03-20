d4720a80893709e6bf7b2cf31a89da0af78af7ed
mihanizm56 <m727507.56@mail.ru>
2022-03-21 00:11:39 +0500

    build(configure-package): update email pattern and update deps

--- lib/patterns/index.ts
+++ lib/patterns/index.ts
@@ -3,7 +3,7 @@ export const decimalNumbers = /^\d*(\.)?\d{1,20}$/;
 export const cyrrillicsOnly = /^[ а-яА-ЯёЁ]*$/;
 // Standart email validation that matches almost all email formats
 // eslint-disable-next-line
-export const emailPattern = /^[a-zA-Zа-яА-Я0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Zа-яА-Я0-9](?:[a-zA-Zа-яА-Я0-9-]{0,61}[a-zA-Zа-яА-Я0-9])?(?:\.[a-zA-Zа-яА-Я0-9](?:[a-zA-Zа-яА-Я0-9-]{0,61}[a-zA-Zа-яА-Я0-9])?)*$/;
+export const emailPattern = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
 export const phoneNumber = /^\+?\d(\(\d{0,3}\)|\d{0,3})(\d{3})(-(\d{2})-(\d{2})|(\d{4}))$/;
 export const datePattern = /^\d{2}\.\d{2}\.\d{4}$/;
 export const isEmptyPattern = /\w+/g;
